33746fa7b6498b9c8724dfb5aea47cf76a9da7c7
non7247 <funi_funi@nifty.com>
2025-12-29 17:22:30 +0900

    feat: 消費系アダプタを実装

--- src/iter.rs
+++ src/iter.rs
@@ -3,6 +3,77 @@ use std::ops;
 pub trait MyIterator {
     type Item;
     fn next(&mut self) -> Option<Self::Item>;
+
+    fn for_each<F>(mut self, mut f: F)
+    where
+        F: FnMut(Self::Item),
+        Self: Sized,
+    {
+        while let Some(x) = self.next() {
+            f(x);
+        }
+    }
+
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        for _ in 0..n {
+            self.next()?;
+        }
+        self.next()
+    }
+
+    fn collect_vec(self) -> Vec<Self::Item> 
+    where
+        Self: Sized,
+    {
+        let mut vec = Vec::new();
+        self.for_each(|x| vec.push(x));
+        vec
+    }
+
+    fn find<P>(&mut self, mut predicate: P) -> Option<Self::Item>
+    where
+        P: FnMut(&Self::Item) -> bool,
+    {
+        while let Some(x) = self.next() {
+            if predicate(&x) {
+                return Some(x);
+            }
+        }
+        None
+    }
+
+    fn fold<B, F>(mut self, mut acc: B, mut f: F) -> B
+    where
+        F: FnMut(B, Self::Item) -> B,
+        Self: Sized,
+    {
+        while let Some(x) = self.next() {
+            acc = f(acc, x);
+        }
+        acc
+    }
+
+    fn sum(self) -> Self::Item
+    where
+        Self::Item: ops::Add<Output = Self::Item> + Default,
+        Self: Sized,
+    {
+        self.fold(Self::Item::default(), |acc, x| acc + x)
+    }
+
+    fn count(self) -> usize
+    where
+        Self: Sized,
+    {
+        self.fold(0, |acc, _| acc + 1)
+    }
+
+    fn last(self) -> Option<Self::Item>
+    where
+        Self: Sized,
+    {
+        self.fold(None, |_, x| Some(x))
+    }
 }
 
 pub struct StdIter<T>(pub T);
--- src/main.rs
+++ src/main.rs
@@ -44,8 +44,34 @@ impl MyIterator for RangeUsize {
 }
 
 fn main() {
-    let mut range = RangeUsize::new(1, 4);
-    while let Some(x) = range.next() {
-        println!("Raw Range: {}", x);
-    }
+    let n = RangeUsize::new(0, 5).count();
+    println!("[count] 0..5 => {}", n);
+
+    print!("[for_each] 0..5 =>");
+    RangeUsize::new(0, 5).for_each(|x| print!(" {}", x));
+    println!();
+
+    let last = RangeUsize::new(10, 15).last();
+    println!("[last] 10..15 => {:?}", last);
+
+    let sum = RangeUsize::new(1, 6).fold(0usize, |acc, x| acc + x);
+    println!("[fold] sum 1..6 => {}", sum);
+
+    let sum = RangeUsize::new(1, 6).sum();
+    println!("[sum] sum 1..6 => {}", sum);
+
+    let v = RangeUsize::new(3, 8).collect_vec();
+    println!("[collect_vec] 3..8 => {:?}", v);
+
+    let mut it = RangeUsize::new(100, 110);
+    let third = it.nth(3);
+    println!("[nth] 100..110 nth(3) => {:?}", third);
+    let next = it.next();
+    println!("[nth] then next() => {:?}", next);
+
+    let mut it = RangeUsize::new(0, 20);
+    let found = it.find(|x| *x % 7 == 0 && *x != 0);
+    println!("[find] first nonzero multiple of 7 in 0..20 => {:?}", found);
+    let after = it.next();
+    println!("[find] then next() => {:?}", after);
 }
